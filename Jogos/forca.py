0d4b979fbc913459bb814c34e1a7b902375cab01
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-03-30 11:50:25 -0300

    Add files via upload

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,28 +1,51 @@
 def jogar_forca():
+    
     print("*******************************")
     print("\nBem vindo ao jogo da forca!\n")
     print("*******************************")
 
+    arquivo = open("Jogos/palavras.txt", "r") #R = apenas read (ler) o arquivo
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+                                                         
+    arquivo.close()
+
     palavra_secreta = "processador"
-    letras_acertadas = ["_","_","_","_","_","_","_","_","_","_","_"]
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    # for letra in palavra_secreta:
+        #letras_acertadas.append("_")
+    
     perdeu = False
     acertou = False
+    erros = 0
 
-    #Enquanto não acertar a palavra secreta, o jogador pode jogar.
-
+    #Enquanto não acertar a palavra secreta e não perder, o jogador pode jogar.
     while not perdeu and not acertou:
-        chute = input("Escreva uma letra: ")
-        chute = chute.strip()
-        #Strip remove a quebra de linha
+        chute = input("\nEscreva uma letra: ")
+        chute = chute.strip().lower() 
 
-        #index define a posição da letra
-        index = 0
+        # Se o chute estiver na palavra, preenchemos as posições corretas
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            #Se não estiver, adicionamos APENAS 1 erro nesta rodada
+            erros += 1 
+        # Verifica se o jogador perdeu
+        perdeu = erros == 6
 
-        for letra in palavra_secreta:
-            if chute.lower() == letra.lower():
-                letras_acertadas[index] = letra
-            index = index + 1
+        acertou = "_"not in letras_acertadas
+        if acertou:
+            print(f'Você acertou a palavra secreta que era {palavra_secreta}')
         print(letras_acertadas)
+
 
 if(__name__ == "__main__"):
     jogar_forca()
